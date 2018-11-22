80fef8518952bb635ee77d8469a4cc502ccb62f9
Dmitry Dolgushin <razor9714@gmail.com>
2018-11-22 23:23:46 +0600

    changed the name of variables, finished the function

--- js/map.js
+++ js/map.js
@@ -5,57 +5,36 @@ var AVATAR_FORMAT = '.png';
 var STICKER_MIN_X = 0;
 var STICKER_MIN_Y = 130;
 var STICKER_MAX_Y = 630;
-var NUMBER_OF_OBJECTS = 8;
-var MIN_PRICE = 1000;
-var MAX_PRICE = 1000000;
-var MIN_TYPES = 0;
-var MAX_TYPES = 3;
-var MIN_ROOMS = 1;
-var MAX_ROOMS = 5;
-var MIN_GUESTS = 1;
-var MAX_GUESTS = 20;
-var MIN_CHECKIN = 0;
-var MAX_CHECKIN = 2;
-var MIN_CHECKOUT = 0;
-var MAX_CHECKOUT = 2;
-var MIN_FEATURES = 0;
-var MAX_FEATURES = 5;
+var PIN_LIMIT = 8;
+var PRICE_MIN = 1000;
+var PRICE_MAX = 1000000;
+var TYPES_MIN = 0;
+var TYPES_MAX = 3;
+var ROOMS_MIN = 1;
+var ROOMS_MAX = 5;
+var GUESTS_MIN = 1;
+var GUESTS_MAX = 20;
+var CHECKIN_MIN = 0;
+var CHECKIN_MAX = 2;
+var CHECKOUT_MIN = 0;
+var CHECKOUT_MAX = 2;
+var FEATURES_MIN = 0;
+var FEATURES_MAX = 5;
 var RANDOM_PHOTO_FROM = 0;
 var RANDOM_PHOTO_TO = 2;
 var STICKER_WIDTH = 50;
 var STICKER_HEIGHT = 70;
 var TEXT_TITLE = 'заголовок объявления';
 
-var adverts = [];
-var houses = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var types = ['palace', 'flat', 'house', 'bungalo'];
-var registrationTimes = ['12:00', '13:00', '14:00'];
-var evictionTimes = ['12:00', '13:00', '14:00'];
-var conditions = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var pictures = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
 var generatingRandomNumber = function (min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
+  return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
-var compareRandom = function () {
-  return Math.random() - 0.5;
-};
+var createPinData = function (i) {
+  var randomCoordinateX = generatingRandomNumber(STICKER_MIN_X, stickerBlockWidth);
+  var randomCoordinateY = generatingRandomNumber(STICKER_MIN_Y, STICKER_MAX_Y);
 
-pictures.sort(compareRandom);
-
-var randomCoordinateX;
-var randomCoordinateY;
-
-var stickerBlockWidth = document.querySelector('.map__overlay').offsetWidth;
-
-for (var i = 0; i < NUMBER_OF_OBJECTS; i++) {
-  randomCoordinateX = generatingRandomNumber(STICKER_MIN_X, stickerBlockWidth);
-  randomCoordinateY = generatingRandomNumber(STICKER_MIN_Y, STICKER_MAX_Y);
-
-  adverts[i] = {
+  return {
     author: {
       avatar: AVATAR_PATH + 'user0' + (i + 1) + AVATAR_FORMAT
     },
@@ -63,13 +42,13 @@ for (var i = 0; i < NUMBER_OF_OBJECTS; i++) {
     offer: {
       title: houses[i],
       address: randomCoordinateX + ', ' + randomCoordinateY,
-      price: generatingRandomNumber(MIN_PRICE, MAX_PRICE),
-      type: types[generatingRandomNumber(MIN_TYPES, MAX_TYPES)],
-      rooms: generatingRandomNumber(MIN_ROOMS, MAX_ROOMS),
-      guests: generatingRandomNumber(MIN_GUESTS, MAX_GUESTS),
-      checkin: registrationTimes[generatingRandomNumber(MIN_CHECKIN, MAX_CHECKIN)],
-      checkout: evictionTimes[generatingRandomNumber(MIN_CHECKOUT, MAX_CHECKOUT)],
-      features: conditions.slice(generatingRandomNumber(MIN_FEATURES, MAX_FEATURES)),
+      price: generatingRandomNumber(PRICE_MIN, PRICE_MAX),
+      type: types[generatingRandomNumber(TYPES_MIN, TYPES_MAX)],
+      rooms: generatingRandomNumber(ROOMS_MIN, ROOMS_MAX),
+      guests: generatingRandomNumber(GUESTS_MIN, GUESTS_MAX),
+      checkin: registrationTimes[generatingRandomNumber(CHECKIN_MIN, CHECKIN_MAX)],
+      checkout: evictionTimes[generatingRandomNumber(CHECKOUT_MIN, CHECKOUT_MAX)],
+      features: conditions.slice(generatingRandomNumber(FEATURES_MIN, FEATURES_MAX)),
       description: '',
       photos: pictures[generatingRandomNumber(RANDOM_PHOTO_FROM, RANDOM_PHOTO_TO)]
     },
@@ -79,21 +58,40 @@ for (var i = 0; i < NUMBER_OF_OBJECTS; i++) {
       y: randomCoordinateY
     }
   };
+};
+
+var createPinElement = function (templateElement, data) {
+  elementButton = templateElement.cloneNode(true);
+  elementButton.style.left = data.location.x - STICKER_WIDTH / 2 + 'px';
+  elementButton.style.top = data.location.y - STICKER_HEIGHT + 'px';
+
+  elementButton.querySelector('img').src = data.author.avatar;
+  elementButton.querySelector('img').alt = TEXT_TITLE;
+
+  return elementButton;
+};
+
+
+var houses = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var types = ['palace', 'flat', 'house', 'bungalo'];
+var registrationTimes = ['12:00', '13:00', '14:00'];
+var evictionTimes = ['12:00', '13:00', '14:00'];
+var conditions = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var pictures = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var mapOverlayElement = document.querySelector('.map__overlay');
+var stickerBlockWidth = mapOverlayElement.offsetWidth;
+var StickerButtonElement = document.querySelector('#pin').content.querySelector('button');
+var stickerElements = document.querySelector('.map__pins');
+var elementButton;
+
+var pinData;
+var pinFragment = document.createDocumentFragment();
+
+for (var i = 0; i < PIN_LIMIT; i++) {
+  pinData = createPinData(i);
+  pinFragment.appendChild(createPinElement(StickerButtonElement, pinData));
+  stickerElements.appendChild(pinFragment);
 }
 
 document.querySelector('.map').classList.remove('map--faded');
-
-var templateStickerButton = document.querySelector('#pin').content.querySelector('button');
-
-var pins = document.querySelector('.map__pins');
-
-for (i = 0; i < adverts.length; i++) {
-  var elementButton = templateStickerButton.cloneNode(true);
-  elementButton.style.left = adverts[i].location.x - STICKER_WIDTH / 2 + 'px';
-  elementButton.style.top = adverts[i].location.y - STICKER_HEIGHT + 'px';
-
-  elementButton.querySelector('img').src = adverts[i].author.avatar;
-  elementButton.querySelector('img').alt = TEXT_TITLE;
-
-  pins.appendChild(elementButton);
-}

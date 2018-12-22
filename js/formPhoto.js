1ebae63ac059aeecbe654439c4983e15c76db03b
Dmitry Dolgushin <razor9714@gmail.com>
2018-12-23 02:15:51 +0600

    change photo upload

--- js/formPhoto.js
+++ js/formPhoto.js
@@ -8,26 +8,59 @@
   var previewMapElement = document.querySelector('.ad-form-header__preview img');
 
   // var fileChooserHouse = document.querySelector('.ad-form__input');
-  // var previewHouse = document.querySelector('.ad-form__photo');
+  var previewHouseElement = document.querySelector('.ad-form__photo');
+  var houseContainerElement = document.querySelector('.ad-form__photo-container');
 
-  var dropZoneElement = document.querySelector('.ad-form-header__drop-zone');
+  var preventDefaults = function (evt) {
+    evt.preventDefault();
+  };
 
-  var handleDrop = function (evt) {
+  ['dragover', 'dragleave', 'drop'].forEach(function (eventName) {
+    document.body.addEventListener(eventName, preventDefaults);
+  });
+
+  var dropZoneMapElement = document.querySelector('.ad-form-header__drop-zone');
+  var dropZoneHouseElement = document.querySelector('.ad-form__drop-zone');
+
+  var handleMapDrop = function (evt) {
     var dt = evt.dataTransfer;
     var files = dt.files;
-    handleFiles(files);
+    handleMapFiles(files);
   };
 
-  var handleFiles = function (files) {
-    files = Array.from(files);
-    files.forEach(previewFile);
+  var handleMapFiles = function (file) {
+    file = Array.from(file);
+    file.forEach(previewMapFile);
   };
 
-  var previewFile = function (file) {
+  var previewMapFile = function (file) {
     var reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = function () {
       previewMapElement.src = reader.result;
+    };
+  };
+
+  var handleHouseDrop = function (evt) {
+    var dt = evt.dataTransfer;
+    var files = dt.files;
+    handleHouseFiles(files);
+  };
+
+  var handleHouseFiles = function (file) {
+    file = Array.from(file);
+    file.forEach(previewHouseFile);
+  };
+
+  var previewHouseFile = function (file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function () {
+      previewHouseElement.remove();
+      var blockPhoto = document.createElement('div');
+      blockPhoto.style.backgroundImage = 'url(' + reader.result + ')';
+      blockPhoto.style.backgroundSize = 'cover';
+      houseContainerElement.appendChild(blockPhoto).classList.add('ad-form__photo');
     };
   };
 
@@ -54,14 +87,28 @@
     photoUpload(fileChooserMapElement, previewMapElement);
   };
 
+  var blocksPhotoRemove = function () {
+    var previewHouseElements = document.querySelectorAll('.ad-form__photo');
+    var blockPhotoDefault = document.createElement('div');
+
+    previewHouseElements.forEach(function (element) {
+      element.remove();
+    });
+
+    houseContainerElement.appendChild(blockPhotoDefault).classList.add('ad-form__photo');
+  };
+
   window.formPhoto = {
     activate: function () {
-      dropZoneElement.addEventListener('drop', handleDrop);
+      dropZoneMapElement.addEventListener('drop', handleMapDrop);
+      dropZoneHouseElement.addEventListener('drop', handleHouseDrop);
       fileChooserMapElement.addEventListener('change', onInputMapPhotoChange);
     },
     deactivate: function () {
       previewMapElement.src = DEFAULT_PHOTO;
-      dropZoneElement.removeEventListener('drop', handleDrop);
+      blocksPhotoRemove();
+      dropZoneMapElement.removeEventListener('drop', handleMapDrop);
+      dropZoneHouseElement.removeEventListener('drop', handleHouseDrop);
       fileChooserMapElement.removeEventListener('change', onInputMapPhotoChange);
     }
   };

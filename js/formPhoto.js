fa03b96387d79c0b9c0ab2111a53ffa39b5df722
Dmitry Dolgushin <razor9714@gmail.com>
2018-12-23 19:52:14 +0600

    correct errors

--- js/formPhoto.js
+++ js/formPhoto.js
@@ -14,9 +14,6 @@
   var dropZoneMapElement = document.querySelector('.ad-form-header__drop-zone');
   var dropZoneHouseElement = document.querySelector('.ad-form__drop-zone');
 
-  var onZoneMapHouseDragoverDrop = function (evt) {
-    evt.preventDefault();
-  };
 
   var createPreviewDrop = function (callback) {
     return function (file) {
@@ -39,15 +36,15 @@
   });
 
   var previewHouseFileDrop = createPreviewDrop(function (content) {
-    housePreviewElement.remove();
-    var blockPhoto = document.createElement('div');
-    blockPhoto.style.backgroundImage = 'url(' + content + ')';
-    blockPhoto.style.backgroundSize = 'cover';
-    houseContainerElement.appendChild(blockPhoto).classList.add('ad-form__photo');
-  });
+    var blockPhotoElement = document.createElement('div');
 
-  var onZoneMapDrop = createImageDropHandler(previewMapFileDrop);
-  var onZoneHouseDrop = createImageDropHandler(previewHouseFileDrop);
+    blockPhotoElement.style.backgroundImage = 'url(' + content + ')';
+    blockPhotoElement.style.backgroundSize = 'cover';
+    blockPhotoElement.classList.add('ad-form__photo');
+
+    housePreviewElement.remove();
+    houseContainerElement.appendChild(blockPhotoElement);
+  });
 
   var createPreviewChange = function (fileChooser, callback) {
     return function () {
@@ -61,60 +58,89 @@
       if (matches) {
         var reader = new FileReader();
 
-        reader.addEventListener('load', function () {
+        reader.onloadend = function () {
           callback(reader.result);
-        });
+        };
 
         reader.readAsDataURL(file);
       }
     };
   };
 
+  var removePhotos = function () {
+    var photoElements = document.querySelectorAll('.ad-form__photo');
+
+    Array.prototype.forEach.call(photoElements, function (element) {
+      element.remove();
+    });
+  };
+
+  var createPhotoPlaceholder = function () {
+    var blockPhotoDefaultElement = document.createElement('div');
+
+    blockPhotoDefaultElement.classList.add('ad-form__photo');
+    houseContainerElement.appendChild(blockPhotoDefaultElement);
+  };
+
+  var preventDefaultHandler = function (evt) {
+    evt.preventDefault();
+  };
+
+  var onZoneMapDrop = createImageDropHandler(previewMapFileDrop);
+  var onZoneHouseDrop = createImageDropHandler(previewHouseFileDrop);
+  var onZoneMapDragover = preventDefaultHandler;
+  var onZoneHouseDragover = preventDefaultHandler;
+
+  var onWindowDragover = preventDefaultHandler;
+  var onWindowDrop = preventDefaultHandler;
+
   var onPreviewMapFileChange = createPreviewChange(mapFileChooserElement, function (content) {
     mapPreviewElement.src = content;
   });
 
   var onPreviewHouseFileChange = createPreviewChange(houseFileChooserElement, function (content) {
+    var photoElement = document.createElement('div');
+
+    photoElement.style.backgroundImage = 'url(' + content + ')';
+    photoElement.style.backgroundSize = 'cover';
+    photoElement.classList.add('ad-form__photo');
+
     housePreviewElement.remove();
-    var blockPhoto = document.createElement('div');
-    blockPhoto.style.backgroundImage = 'url(' + content + ')';
-    blockPhoto.style.backgroundSize = 'cover';
-    houseContainerElement.appendChild(blockPhoto).classList.add('ad-form__photo');
+    houseContainerElement.appendChild(photoElement);
   });
 
-  var blocksPhotoRemove = function () {
-    var previewHouseElements = document.querySelectorAll('.ad-form__photo');
-    var blockPhotoDefault = document.createElement('div');
-
-    previewHouseElements.forEach(function (element) {
-      element.remove();
-    });
-
-    houseContainerElement.appendChild(blockPhotoDefault).classList.add('ad-form__photo');
-  };
 
   window.formPhoto = {
     activate: function () {
       housePreviewElement = document.querySelector('.ad-form__photo');
 
-      dropZoneMapElement.addEventListener('drop', onZoneMapHouseDragoverDrop);
-      dropZoneHouseElement.addEventListener('drop', onZoneMapHouseDragoverDrop);
-      dropZoneMapElement.addEventListener('dragover', onZoneMapHouseDragoverDrop);
-      dropZoneHouseElement.addEventListener('dragover', onZoneMapHouseDragoverDrop);
-
       dropZoneMapElement.addEventListener('drop', onZoneMapDrop);
       dropZoneHouseElement.addEventListener('drop', onZoneHouseDrop);
+      dropZoneMapElement.addEventListener('dragover', onZoneMapDragover);
+      dropZoneHouseElement.addEventListener('dragover', onZoneHouseDragover);
+
       mapFileChooserElement.addEventListener('change', onPreviewMapFileChange);
       houseFileChooserElement.addEventListener('change', onPreviewHouseFileChange);
+
+      window.addEventListener('dragover', onWindowDragover);
+      window.addEventListener('drop', onWindowDrop);
     },
     deactivate: function () {
       mapPreviewElement.src = DEFAULT_PHOTO;
-      blocksPhotoRemove();
+
+      removePhotos();
+      createPhotoPlaceholder();
 
       dropZoneMapElement.removeEventListener('drop', onZoneMapDrop);
       dropZoneHouseElement.removeEventListener('drop', onZoneHouseDrop);
+      dropZoneMapElement.addEventListener('dragover', onZoneMapDragover);
+      dropZoneHouseElement.addEventListener('dragover', onZoneHouseDragover);
+
       mapFileChooserElement.removeEventListener('change', onPreviewMapFileChange);
       houseFileChooserElement.removeEventListener('change', onPreviewHouseFileChange);
+
+      window.removeEventListener('dragover', onWindowDragover);
+      window.removeEventListener('drop', onWindowDrop);
     }
   };
 })();
